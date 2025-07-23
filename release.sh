c7ba285f7d768894b17e7fdf0453b8b2d042371e
Balaji Ganesan <bganesan@nvidia.com>
2025-07-23 09:32:52 -0700

    Removed file committed by mistake.

--- release.sh
+++
@@ -1,9 +0,0 @@
-VERSION=0.20.0
-git tag ${VERSION}
-git push origin ${VERSION}
-export NGC_TARGET="api.ngc.nvidia.com";
-export NGC_CLI_API_URL="https://api.ngc.nvidia.com";
-export NGC_CLI_API_KEY="ZjRvYm5lbjVrcGhpamZvcTI5NHFnY3Rna3Y6MWJlOWEyYjktZWY1YS00ZjYxLThiMTctMTM3NzI1YTZlZThm"
-helm package deploy
-ngc registry chart push nv-ngc-devops/nvcf-container-cache:${VERSION} --org nv-ngc-devops
-
