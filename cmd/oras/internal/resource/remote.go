654f31b63b67559205cb5d7db7e387ca1ed37f57
Terry Howe <terrylhowe@gmail.com>
2025-07-28 09:12:18 -0600

    refactor: move properties to new file
    Signed-off-by: Terry Howe <terrylhowe@gmail.com>

--- cmd/oras/internal/resource/remote.go
+++ cmd/oras/internal/resource/remote.go
@@ -2,125 +2,18 @@ package resource
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"net"
 	"net/http"
 	"oras.land/oras-go/v2/registry/remote"
 	"oras.land/oras-go/v2/registry/remote/auth"
 	"oras.land/oras-go/v2/registry/remote/credentials"
 	"oras.land/oras-go/v2/registry/remote/retry"
 	"oras.land/oras/internal/credential"
-	"oras.land/oras/internal/crypto"
-	onet "oras.land/oras/internal/net"
 	"oras.land/oras/internal/trace"
 	"oras.land/oras/internal/version"
-	"strconv"
 	"strings"
 )
-
-// RemoteProperties contains all the attributes to access one registry.
-type RemoteProperties struct {
-	RawReference     string
-	Registry         string
-	Repository       string
-	Reference        string
-	Path             string
-	RemoteRepository *remote.Repository
-
-	CACertFilePath  string
-	CertFilePath    string
-	KeyFilePath     string
-	Insecure        bool
-	PlainHTTP       bool
-	Configs         []string
-	Username        string
-	SecretFromStdin bool
-	Secret          string
-
-	ResolveFlag  []string
-	HeaderFlags  []string
-	ReferrersAPI string
-}
-
-// IsPlainHttp returns the plain http flag for a given registry.
-func (prop *RemoteProperties) IsPlainHttp() bool {
-	if prop.PlainHTTP {
-		return true
-	}
-	host, _, _ := net.SplitHostPort(prop.Registry)
-	if host == "localhost" || prop.Registry == "localhost" {
-		// not specified, defaults to plain http for localhost
-		return true
-	}
-	return false
-}
-
-func (prop *RemoteProperties) IsReferrersSet() bool {
-	return prop.ReferrersAPI == ""
-}
-
-// tlsConfig assembles the tls config.
-func (prop *RemoteProperties) tlsConfig() (*tls.Config, error) {
-	config := &tls.Config{
-		InsecureSkipVerify: prop.Insecure,
-	}
-	if prop.CACertFilePath != "" {
-		var err error
-		config.RootCAs, err = crypto.LoadCertPool(prop.CACertFilePath)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if prop.CertFilePath != "" && prop.KeyFilePath != "" {
-		cert, err := tls.LoadX509KeyPair(prop.CertFilePath, prop.KeyFilePath)
-		if err != nil {
-			return nil, err
-		}
-		config.Certificates = []tls.Certificate{cert}
-	}
-	return config, nil
-}
-
-// parseResolve parses resolve flag.
-func (r *RemoteProperties) parseResolve(baseDial onet.DialFunc) (onet.DialFunc, error) {
-	if len(r.ResolveFlag) == 0 {
-		return baseDial, nil
-	}
-
-	formatError := func(param, message string) error {
-		return fmt.Errorf("failed to parse resolve flag %q: %s", param, message)
-	}
-	var dialer onet.Dialer
-	for _, r := range r.ResolveFlag {
-		parts := strings.SplitN(r, ":", 4)
-		length := len(parts)
-		if length < 3 {
-			return nil, formatError(r, "expecting host:port:address[:address_port]")
-		}
-		host := parts[0]
-		hostPort, err := strconv.Atoi(parts[1])
-		if err != nil {
-			return nil, formatError(r, "expecting uint64 host port")
-		}
-		// ipv6 zone is not parsed
-		address := net.ParseIP(parts[2])
-		if address == nil {
-			return nil, formatError(r, "invalid IP address")
-		}
-		addressPort := hostPort
-		if length > 3 {
-			addressPort, err = strconv.Atoi(parts[3])
-			if err != nil {
-				return nil, formatError(r, "expecting uint64 address port")
-			}
-		}
-		dialer.Add(host, hostPort, address, addressPort)
-	}
-	dialer.BaseDialContext = baseDial
-	return dialer.DialContext, nil
-}
 
 type Remote struct {
 	RemoteProperties
